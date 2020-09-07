7821bb8dcf4e4e57a66418bbf3fdf95153db184d
Chaudhra <47132229+Chaudhra@users.noreply.github.com>
2020-09-07 11:55:31 -0400

    fixed formatting

--- src/Tests/Components/SearchBar.test.js
+++ src/Tests/Components/SearchBar.test.js
@@ -5,13 +5,11 @@ import SearchBar from '../../Components/SearchBar'
 describe('<SearchBar />', () => {
   it('should render SearchBar correctly', () => {
     const wrapper = shallow(<SearchBar />)
-
     expect(wrapper).toMatchSnapshot()
   })
 
   it('should display input bar', () => {
     const wrapper = shallow(<SearchBar />)
-
     expect(wrapper.find('input').text()).toBe('')
   })
 })
